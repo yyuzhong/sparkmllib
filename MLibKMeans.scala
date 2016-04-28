bfbae43732b938c29487e1696a1196cfc3bc957e
Yuzhong Yan <yyuzhong@hotmail.com>
2016-04-28 08:50:24 -0500

    Remove test codes

--- MLibKMeans.scala
+++ MLibKMeans.scala
@@ -15,17 +15,16 @@ val WSSSE = clusters.computeCost(parsedData)
 println("WSSSE = " + WSSSE)
 
 
-clusters.k
-
-clusters.clusterCenters.zipWithIndex
+// clusters.k
+// clusters.clusterCenters.zipWithIndex
 
 val results = parsedData.map(point=> {val prediction=clusters.predict(point);(point.toString,prediction)})
 print(results.collect().toList);
 
-    clusters.predict(Vectors.dense(Array(60.0,104.0)))
+clusters.predict(Vectors.dense(Array(60.0,104.0)))
 
-    clusters.predict(Vectors.dense(Array(40.0,54.0)))
+clusters.predict(Vectors.dense(Array(40.0,54.0)))
 
 
-val inputData = sc.textFile("hdfs://129.207.46.225:8020/mlclass/kmeans_short.csv")
-val inputData = sc.textFile("hdfs://129.207.46.225:8020/mlclass/kmeans_iris.csv")
+//val inputData = sc.textFile("hdfs://129.207.46.225:8020/mlclass/kmeans_short.csv")
+//val inputData = sc.textFile("hdfs://129.207.46.225:8020/mlclass/kmeans_iris.csv")
